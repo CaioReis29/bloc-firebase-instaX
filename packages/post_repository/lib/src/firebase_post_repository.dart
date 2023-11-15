3b90ad0002cb31347c4c8100f6ee390d964a5327
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-11-14 21:09:42 -0300

    feat: add user model and user entity

--- packages/post_repository/lib/src/firebase_post_repository.dart
+++ packages/post_repository/lib/src/firebase_post_repository.dart
@@ -1,3 +1,3 @@
-import '../user_repository.dart';
+import 'package:post_repository/post_repository.dart';
 
 class FirebasePostRepository implements PostRepository {}
--- packages/user_repository/lib/src/models/my_user.dart
+++ packages/user_repository/lib/src/models/my_user.dart
@@ -1,1 +1,58 @@
-class MyUser {}
+import 'package:equatable/equatable.dart';
+import 'package:user_repository/src/entities/entities.dart';
+
+class MyUser extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  const MyUser({
+    required this.id,
+    required this.email,
+    required this.name,
+    this.picture,
+  });
+
+  // usuário não encontrado
+
+  static const empty = MyUser(
+    id: '',
+    email: '',
+    name: '',
+  );
+
+  // modificar parâmetros de usuário
+
+  MyUser copyWith({
+    String? id,
+    String? email,
+    String? name,
+    String? picture,
+  }) {
+    return MyUser(
+      id: id ?? "",
+      email: email ?? "",
+      name: name ?? "",
+    );
+  }
+
+  // getter em caso de usuário estar vazio
+
+  bool get isEmpty => this == MyUser.empty;
+
+  // getter em caso de usuário não estar vazio
+
+  bool get isNotEmpty => this != MyUser.empty;
+
+  MyUserEntity toEntity() {
+    return MyUserEntity(
+      id: id,
+      email: email,
+      name: name,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, email, name, picture];
+}
