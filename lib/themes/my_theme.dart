b446f096822d5b5019cd2f02287423c0708da008
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-11-17 12:27:46 -0300

    style: APP theme created

---
+++ lib/themes/my_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+ThemeData myTheme = ThemeData(
+  colorScheme: const ColorScheme.light(
+    background: Colors.white,
+    onBackground: Colors.white,
+    primary: Color.fromRGBO(206, 147, 216, 1),
+    secondary: Colors.black,
+    onSecondary: Color.fromRGBO(244, 143, 177, 1),
+    tertiary: Color.fromRGBO(255, 204, 128, 1),
+    error: Colors.red,
+    outline: Color(0xFF424242),
+  ),
+);
