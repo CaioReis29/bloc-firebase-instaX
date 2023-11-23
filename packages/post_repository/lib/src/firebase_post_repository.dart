65b716f3ea253ed6540025ba4cdf33c7138aca5b
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-11-23 17:26:55 -0300

    feat: firestore functions in post repository created

--- packages/post_repository/lib/src/firebase_post_repository.dart
+++ packages/post_repository/lib/src/firebase_post_repository.dart
@@ -1,3 +1,42 @@
-import 'package:post_repository/post_repository.dart';
+import 'dart:developer';
 
-class FirebasePostRepository implements PostRepository {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:post_repository/post_repository.dart';
+import 'package:uuid/uuid.dart';
+
+class FirebasePostRepository implements PostRepository {
+  final postCollection = FirebaseFirestore.instance.collection('posts');
+
+  @override
+  Future<Post> createPost(Post post) async {
+    try {
+      post.postId = const Uuid().v1();
+      post.createdAt = DateTime.now();
+
+      await postCollection.doc(post.postId).set(
+            post.toEntity().toDocument(),
+          );
+
+      return post;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<Post>> getPost() async {
+    try {
+      return postCollection.get().then(
+            (value) => value.docs
+                .map((e) => Post.fromEntity(PostEntity.fromDocument(
+                      e.data(),
+                    )))
+                .toList(),
+          );
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+}
--- packages/post_repository/lib/src/post_repo.dart
+++ packages/post_repository/lib/src/post_repo.dart
@@ -1,1 +1,7 @@
-abstract class PostRepository {}
+import 'package:post_repository/post_repository.dart';
+
+abstract class PostRepository {
+  Future<Post> createPost(Post post);
+
+  Future<List<Post>> getPost();
+}
